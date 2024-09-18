02e1a976b6918406f8362d4932d3c2365f30363e
MiguelMedeiros123 <161078186+MiguelMedeiros123@users.noreply.github.com>
2024-09-18 21:57:41 +0000

    mais projeto

--- Projeto/models.py
+++ Projeto/models.py
@@ -3,7 +3,7 @@ import datetime as dt
 
 
 class Funcionario:
-    def __init__(self, id: int, nome: str, ocup: str, nasc: dt.datetime, cpf: str, email: str, custo: float, contr: dt.datetime, id_setor: int) -> None:
+    def __init__(self, id: int, nome: str, ocup: str, nasc: dt.date, cpf: str, email: str, custo: float, contr: dt.date, id_setor: int) -> None:
         self.id = id
         self.nome = nome
         self.ocup = ocup
@@ -15,10 +15,20 @@ class Funcionario:
         self.id_setor = id_setor
 
     def __str__(self) -> str:
-        return f"{self.id} - {self.nome} - {self.ocup}; Nascimento - {dt.datetime.strftime(self.nasc, "%d/%M/%Y")}; CPF - {self.cpf}; E-mail - {self.email}; Custo mensal - {self.custo}; Data de contratação: {dt.datetime.strftime(self.contr, "%d/%M/%Y")}; ID do setor: {self.id_setor}"
+        return f"{self.id} - {self.nome} - {self.ocup}; Nascimento - {dt.date.strftime(self.nasc, "%d/%m/%Y")}; CPF - {self.cpf}; E-mail - {self.email}; Custo mensal - {self.custo}; Data de contratação: {dt.date.strftime(self.contr, "%d/%m/%Y")}; ID do setor: {self.id_setor}"
     
     def to_json(self):
         dic = {}
+        dic["id"] = self.id
+        dic["nome"] = self.nome
+        dic["ocup"] = self.ocup
+        dic["nasc"] = dt.datetime.strftime(self.nasc, "%d/%m/%Y")
+        dic["cpf"] = self.cpf
+        dic["email"] = self.email
+        dic["custo"] = self.custo
+        dic["contr"] = dt.datetime.strftime(self.contr, "%d/%m/%Y")
+        dic["id_setor"] = self.id_setor
+        return dic
 
 
 class Funcionarios:
@@ -51,7 +61,6 @@ class Funcionarios:
         cls.abrir()
         f = cls.listar_id(obj.id)
         if f != None:
-            f.id = id
             f.nome = obj.nome
             f.ocup = obj.ocup
             f.nasc = obj.nasc
@@ -74,34 +83,53 @@ class Funcionarios:
     def abrir(cls):
         cls.objetos = []
         try:
-            with open("objetos.json", mode = "r") as arquivo:
+            with open("funcionarios.json", mode = "r") as arquivo:
                 texto = json.load(arquivo)
                 for obj in texto:
-                    n = Funcionario(obj["id"], obj["nome"], obj["email"], dt.datetime.strptime(obj["nasc"], "%d/%m/%Y")adsadasfasdfdsjkfnsdjkfsdbjkhnfbhnjkafjbhkabhjkn)
+                    n = Funcionario(obj["id"], obj["nome"], obj["email"], dt.date.strptime(obj["nasc"], "%d/%m/%Y")adsadasfasdfdsjkfnsdjkfsdbjkhnfbhnjkafjbhkabhjkn)
                     cls.objetos.append(n)
         except FileNotFoundError:
             pass
 
     @classmethod
     def salvar(cls):
-        with open("objetos.json", mode = "w") as arquivo:
+        with open("funcionarios.json", mode = "w") as arquivo:
             json.dump(cls.objetos, arquivo, default = Funcionario.to_json)
 
 
 class Setor:
-    def __init__(self, id: int, nome: str, desc: str, data: dt.datetime) -> None:
+    def __init__(self, id: int, nome: str, desc: str, data: dt.date) -> None:
         self.id = id
         self.nome = nome
         self.desc = desc
         self.data = data
         self.funcionarios = []
         self.custo = 0
+
     def __str__(self) -> str:
         return f"{self.id} - {self.nome}; {self.desc}; Criado a {self.data}; Funcionários - {len(self.funcionarios)}; Custo mensal - {self.custo}"
+    
+    def to_json(self):
+        dic = {}
 
 
 class Setores:
     objetos = []
+    
+    @classmethod
+    def inserir(cls, obj: Setor):
+        cls.abrir()
+        m = 0
+        for s in cls.objetos:
+            if s.id > m: m = s.id
+        obj.id = m
+        cls.objetos.append(obj)
+        cls.salvar()
+
+    @classmethod
+    def listar(cls):
+        cls.abrir()
+        return cls.objetos
 
     @classmethod
     def listar_id(cls, id: int) -> Setor:
@@ -109,6 +137,24 @@ class Setores:
         for s in cls.objetos:
             if s.id == id: return s
         return None
+    
+    @classmethod
+    def atualizar(cls, obj: Setor):
+        cls.abrir()
+        s = cls.listar_id(obj.id)
+        if s != None:
+            s.nome = obj.nome
+            s.desc = obj.desc
+            s.data = obj.data
+        cls.salvar()
+    
+    @classmethod
+    def excluir(cls, id):
+        cls.abrir()
+        s = cls.listar_id(id)
+        if s != None:
+            cls.objetos.remove(s)
+            cls.salvar()
 
     @classmethod
     def inserir_func(cls, id: int, id_func: int):
@@ -120,3 +166,31 @@ class Setores:
                 s.funcionarios.append(id_func)
                 s.custo += f.custo
                 cls.salvar()
+    
+    @classmethod
+    def excluir_func(cls, id: int, id_func: int):
+        cls.abrir()
+        s = cls.listar_id(id)
+        if s != None:
+            f = Funcionarios.listar_id(id_func)
+            if f.id in s.funcionarios:
+                s.funcionarios.remove(f.id)
+                s.custo -= f.custo
+                cls.salvar()
+
+    @classmethod
+    def abrir(cls):
+        cls.objetos = []
+        try:
+            with open("funcionarios.json", mode = "r") as arquivo:
+                texto = json.load(arquivo)
+                for obj in texto:
+                    n = Setor(obj["id"], obj["nome"], obj["email"], dt.date.strptime(obj["nasc"], "%d/%m/%Y")adsadasfasdfdsjkfnsdjkfsdbjkhnfbhnjkafjbhkabhjkn)
+                    cls.objetos.append(n)
+        except FileNotFoundError:
+            pass
+
+    @classmethod
+    def salvar(cls):
+        with open("setores.json", mode = "w") as arquivo:
+            json.dump(cls.objetos, arquivo, default = Setor.to_json)
--- Projeto/views.py
+++ Projeto/views.py
@@ -1,43 +1,17 @@
 from models import Funcionario, Funcionarios
 import datetime as dt
 
-class views:
-    @staticmethod
-    def Funcionario_inserir():
-        nome = input("Informe o nome: ")
-        ocup = input("Informe a ocupação: ")
-        nasc = dt.datetime.strptime(input("Informe a data de nascimento (formato dd/mm/aaaa): "), "%d/%M/%Y")
-        cpf = input("Informe o CPF: ")
-        email = input("Informe o e-mail: ")
-        custo = input("Informe o custo mensal: ")
-        contr = dt.datetime.strptime(input("Informe a data de contratação (formato dd/mm/aaaa): "), "%d/%M/%Y")
-        views.setor_listar(kjjjjjjjjjjjjjj) #abahshdashdahjasdhjasbhjdashbj
-        id_setor = int(input("Informe o ID do setor em que estará o funcionário: "))
-        c = Funcionario(0, nome, ocup, nasc, cpf, email, custo, contr, id_setor)
-        Funcionarios.inserir(c)
 
-    @staticmethod
-    def funcionario_listar():
-        for c in Funcionarios.listar():
-            print(c)
+def Funcionario_inserir(nome: str, ocup: str, nasc: dt.date, cpf: int, email: str, custo: str, contr: dt.date, id_setor: int):
+    c = Funcionario(0, nome, ocup, nasc, cpf, email, custo, contr, id_setor)
+    Funcionarios.inserir(c)
 
-    @staticmethod
-    def funcionario_atualizar():
-        views.funcionario_listar()
-        id = int(input("Informe o ID do funcionário a ser atualizado: "))
-        nome = input("Informe o novo nome: ")
-        ocup = input("Informe a nova ocupação: ")
-        nasc = dt.datetime.strptime(input("Informe a nova data de nascimento (formato dd/mm/aaaa): "), "%d/%M/%Y")
-        cpf = input("Informe o novo CPF: ")
-        email = input("Informe o novo e-mail: ")
-        custo = input("Informe o novo custo mensal: ")
-        contr = dt.datetime.strptime(input("Informe a nova data de contratação (formato dd/mm/aaaa): "), "%d/%M/%Y")
-        id_setor = int(input("Informe o ID do setor ao qual transferir-se-á o funcionário: "))
-        c = Funcionario(id, nome, ocup, nasc, cpf, email, custo, contr, id_setor)
-        Funcionarios.atualizar(c)
+def funcionario_listar():
+    return Funcionarios.listar()
 
-    @staticmethod
-    def funcionario_excluir():
-        views.Funcionario_listar()
-        id = int(input("Informe o id do Funcionario a ser excluído: "))
-        Funcionarios.excluir(id)
+def funcionario_atualizar(id: int, nome: str, ocup: str, nasc: dt.date, cpf: int, email: str, custo: str, contr: dt.date, id_setor: int):
+    c = Funcionario(id, nome, ocup, nasc, cpf, email, custo, contr, id_setor)
+    Funcionarios.atualizar(c)
+
+def funcionario_excluir(id):
+    Funcionarios.excluir(id)
