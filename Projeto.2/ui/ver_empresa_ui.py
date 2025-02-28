48af72b017400b377d356b69333f21f78326b696
MiguelMedeiros123 <161078186+MiguelMedeiros123@users.noreply.github.com>
2025-02-28 19:36:15 +0000

    fim

--- Projeto.2/ui/ver_empresa_ui.py
+++ Projeto.2/ui/ver_empresa_ui.py
@@ -124,7 +124,24 @@ class VerEmpresaUI:
                             st.dataframe(graph, column_config = {"id": "ID", "nome": "Nome", "ocup": "Ocupação", "email": "E-Mail", "contr": "Contratação", "setor": "Setor"}, hide_index=True)
 
     def custos():
-        pass
+        if "empresa_id" not in st.session_state: st.write("Escolhe a empresa.")
+        else:
+            e = view.empresa_listar_id(st.session_state["empresa_id"])
+            if e == None:
+                st.subheader("Indisponível")
+                st.write("O administrador deve inserir-te no sistema da empresa.")
+            else:
+                st.subheader("Setores da empresa")
+                setores = view.empresa_setores(e.get_id())
+                if setores == []: st.write("Não há setor cadastrado na empresa.")
+                else:
+                    lcusto = []
+                    lnome = []
+                    for s in setores:
+                        lcusto.append(view.setor_custo(s.get_id()))
+                        lnome.append(s.get_nome())
+                    graph = pd.DataFrame(lcusto, columns=lnome) 
+                    st.bar_chart(graph) 
     
     def reajustes():
         if "empresa_id" not in st.session_state: st.write("Escolhe a empresa.")
@@ -142,7 +159,7 @@ class VerEmpresaUI:
                         if view.empresa_setores(e.get_id()) == []: st.write("Não há setores cadastrados na empresa")
                         else:
                             percentual = st.text_input("Informa o percentual do reajuste nos gastos dos setores (ex. ±XX.X)")
-                            if st.button("Reajustar"):
+                            if st.button("Reajustar custos"):
                                 try:
                                     view.empresa_reajuste_gastos(e.get_id(), float(percentual))
                                     st.success("Custos reajustados.")
@@ -152,15 +169,16 @@ class VerEmpresaUI:
                                     st.error(erro)
                     elif op == "Funcionários da empresa":
                         if view.empresa_setores(e.get_id()) == []: st.write("Não há setores cadastrados na empresa")
-                        percentual = st.text_input("Informa o percentual do reajuste nos custos dos funcionarios (ex. ±XX.X)")
-                        if st.button("Reajustar"):
-                            try:
-                                view.empresa_reajuste_salarial(e.get_id(), float(percentual))
-                                st.success("Salários reajustados.")
-                                time.sleep(2)
-                                st.rerun()
-                            except Exception as erro:
-                                st.error(erro)
+                        else:
+                            percentual = st.text_input("Informa o percentual do reajuste nos custos dos funcionarios (ex. ±XX.X)")
+                            if st.button("Reajustar custo"):
+                                try:
+                                    view.empresa_reajuste_salarial(e.get_id(), float(percentual))
+                                    st.success("Salários reajustados.")
+                                    time.sleep(2)
+                                    st.rerun()
+                                except Exception as erro:
+                                    st.error(erro)
                     elif op == "Funcionários dum setor":
                         percentual = st.text_input("Informa o percentual do reajuste nos custos dos funcionarios (ex. ±XX.X)")
                         setores = view.empresa_setores(e.get_id())
@@ -179,4 +197,33 @@ class VerEmpresaUI:
                                     st.error(erro)
 
     def realocamentos():
-        pass
+        if st.session_state["conta_nome"] != "admin": st.write("Esta operação é exclusiva ao administrador.")
+        else:
+            op = st.selectbox("Informa o tipo de realocamento", ["Setores para empresa", "Funcionários para setor"])
+            if op == "Setores para empresa":
+                emp = view.empresa_listar()
+                ei = st.selectbox("Informa a empresa da qual sairão os setores", emp)
+                ef = st.selectbox("Informa a empresa para à qual irão os setores", emp)
+                if st.button("Realocar setores"):
+                    if ei == ef: st.error("Não se pode repetir a empresa")
+                    else:
+                        try:
+                            view.multi_setor_mover_empresa(ei.get_id(), ef.get_id())
+                            st.success("Setores movidos")
+                            time.sleep(2)
+                            st.rerun()
+                        except Exception as erro:
+                            st.error(erro)
+            elif op == "Funcionários para setor":
+                seto = view.setor_listar()
+                si = st.selectbox("Informa o setor do qual sairão os funcionários", seto)
+                sf = st.selectbox("Informa a setor ao qual irão os funcionários", seto)
+                ocup = st.text_input("Filtro de ocupação (opcional)")
+                if st.button("Realocar funcionários"):
+                    try:
+                        view.multi_funcionario_mover_setor(ocup, si.get_id(), sf.get_id())
+                        st.success("Funcionários movidos.")
+                        time.sleep(2)
+                        st.rerun()
+                    except Exception as erro:
+                        st.error(erro)
